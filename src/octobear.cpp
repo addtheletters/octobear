2b50610e3798920dbc60e1c178d343fe3cc7a579
addtheletters <bzhang.2003@gmail.com>
2014-09-04 14:14:41 -0400

    sample

--- src/octobear.cpp
+++ src/octobear.cpp
@@ -1,15 +1,16 @@
-//============================================================================
-// Name        : octobear.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
+#include <opencv2/opencv.hpp>
 #include <iostream>
+using namespace cv;
 using namespace std;
-
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int main(int argc, char** argv)
+{
+  Mat im = imread(argc == 2 ? argv[1] : "lenna.png", 1);
+  if (im.empty())
+  {
+    cout << "Cannot open image!" << endl;
+    return -1;
+  }
+  imshow("image", im);
+  waitKey(0);
+  return 0;
 }
