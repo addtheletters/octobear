8676ecad020f7da89308ec5f21e6ba2846232313
dhlanm <dhlanm@gmail.com>
2014-10-01 13:07:55 -0400

    It works
    lol what does it do

--- src/featuredetecttest.cpp
+++ src/featuredetecttest.cpp
@@ -14,37 +14,43 @@ void readme();
 /** @function main */
 int main( int argc, char** argv )
 {
-  if( argc != 3 )
-  { readme(); return -1; }
+  //if( argc != 3 )
+  //{ readme(); return -1; }
 
-  Mat img_1 = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-  Mat img_2 = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
+  Mat img_1 = imread( "test.jpg", CV_LOAD_IMAGE_GRAYSCALE);//argv[1], CV_LOAD_IMAGE_GRAYSCALE );
+  Mat img_2 = imread( "frog.jpg", CV_LOAD_IMAGE_GRAYSCALE);//argv[2], CV_LOAD_IMAGE_GRAYSCALE );
 
   if( !img_1.data || !img_2.data )
   { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
 
   //-- Step 1: Detect the keypoints
-  cv::SimpleBlobDetector::Params params;
-  params.minDistBetweenBlobs = 100.0f;
-  params.filterByInertia = false;
-  params.filterByConvexity = false;
-  params.filterByColor = true;
-  params.filterByCircularity = false;
-  params.filterByArea = false;
-  params.blobColor = 255;
-  //params.minArea = 300.0f;
-  //params.maxArea = 1000.0f;
+  // set up the parameters (check the defaults in opencv's code in blobdetector.cpp)
+    cv::SimpleBlobDetector::Params params;
+    params.minDistBetweenBlobs = 50.0f;
+    params.filterByInertia = false;
+    params.filterByConvexity = false;
+    params.filterByColor = false;
+    params.filterByCircularity = false;
+    params.filterByArea = true;
+    params.minArea = 20.0f;
+    params.maxArea = 500.0f;
+    // ... any other params you don't want default value
 
-  SimpleBlobDetector detector( params );
+    // set up and create the detector using the parameters
+    cv::Ptr<cv::FeatureDetector> blob_detector = new cv::SimpleBlobDetector(params);
+    blob_detector->create("SimpleBlob");
+
+    // detect!
+
 
   std::vector<KeyPoint> keypoints_1, keypoints_2;
 
   cout << "good" << endl;
 
-  detector.detect( img_1, keypoints_1 );
+  blob_detector->detect( img_1, keypoints_1 );
   cout << "good" << endl;
 
-  detector.detect( img_2, keypoints_2 );
+  blob_detector->detect( img_2, keypoints_2 );
   cout << "good" << endl;
 
 
