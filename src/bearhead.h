68fda1e259d838c105e0b475296b21177aa57d3a
tomatocurry1 <lee123456771@gmail.com>
2014-11-18 12:55:28 -0500

    header and octobear loadImage error fix

--- src/bearhead.h
+++ src/bearhead.h
@@ -8,6 +8,9 @@
 #ifndef BEARHEAD_H_
 #define BEARHEAD_H_
 
+#include "opencv/cv.h"
+
+
 int colorchoose(int argc, char** argv);
 int featuredetect(int argc, char** argv);
 int greendetect(int argc, char** argv);
@@ -16,5 +19,6 @@ int tracking(int argc, char** argv);
 int unworked_triag(int argc, char** argv);
 int twocams(int argc, char *argv[], char *window_name);
 int twostreams(int argc, char** argv) ;
+void loadImages(char* img_n1, char* img_n2 , cv::Mat* img1, cv::Mat* img2);
 
 #endif /* BEARHEAD_H_ */
--- src/octobear.cpp
+++ src/octobear.cpp
@@ -2,7 +2,17 @@
 // rawr bearhz
 
 #include "bearhead.h"
+#include "iostream"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+
+using namespace cv;
+using namespace std;
+
+Mat img1, img2;
 
 int main( int argc, char** argv ){
+	loadImages("dot1.png", "dot2.png", &img1, &img2);
 	kalmanrob(argc, argv);
 }
