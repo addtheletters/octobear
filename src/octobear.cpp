2259b65ae0603f9319afc0e8f80514fe9a962ce7
addtheletters <bzhang.2003@gmail.com>
2014-12-05 10:45:25 -0500

    linear thing function

--- src/octobear.cpp
+++ src/octobear.cpp
@@ -16,12 +16,12 @@ int main( int argc, char** argv ){
 	//loadImages(argc, argv, &img1, &img2);
 	//kalmanrob(argc, argv);
 
-	sample_SBM(argc, argv);
+	//sample_SBM(argc, argv);
 	//sample_calibration(argc, argv);
 	//string args[2] = {"sc1.jpg", "sc2.jpg"};
 
 	//featuredetect(argc, argv);
-	//unworked_triag(argc, argv);
+	unworked_triag(argc, argv);
 
 	//sample_stereo_calibration(argc, argv);
 
--- src/triangluarZasion.cpp
+++ src/triangluarZasion.cpp
@@ -1,10 +1,6 @@
-/*
- * Framing credit to Shermal Fernando
- * http://opencv-srf.blogspot.com/2010/09/object-detection-using-color-seperation.html
- *
- */
 
 #include <iostream>
+#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
@@ -16,6 +12,32 @@ void putPointIn1DMat(Mat m, int index, int px, int py){
 	m.at<Vec2d>(0, index)[0] = px;
 	m.at<Vec2d>(0, index)[1] = py;
 }
+
+Mat_<double> LinearLSTriangulation(Point3d u,   	//homogenous image point (u,v,1)
+                   Matx34d P,   	//camera 1 matrix
+                   Point3d u1,  	//homogenous image point in 2nd camera
+                   Matx34d P1   	//camera 2 matrix
+                                   )
+{
+    //build matrix A for homogenous equation system Ax = 0
+    //assume X = (x,y,z,1), for Linear-LS method
+    //which turns it into a AX = B system, where A is 4x3, X is 3x1 and B is 4x1
+    Matx43d A(u.x*P(2,0)-P(0,0),	u.x*P(2,1)-P(0,1),  	u.x*P(2,2)-P(0,2),
+          u.y*P(2,0)-P(1,0),	u.y*P(2,1)-P(1,1),  	u.y*P(2,2)-P(1,2),
+          u1.x*P1(2,0)-P1(0,0), u1.x*P1(2,1)-P1(0,1),   u1.x*P1(2,2)-P1(0,2),
+          u1.y*P1(2,0)-P1(1,0), u1.y*P1(2,1)-P1(1,1),   u1.y*P1(2,2)-P1(1,2)
+              );
+    Matx41d B(	-(u.x*P(2,3)	-P(0,3)),
+                      -(u.y*P(2,3)  -P(1,3)),
+                      -(u1.x*P1(2,3)	-P1(0,3)),
+                      -(u1.y*P1(2,3)	-P1(1,3)));
+
+    Mat_<double> X;
+    solve(A,B,X,DECOMP_SVD);
+
+    return X;
+}
+
 
 int unworked_triag(int argc, char** argv) {
 	//[ 6.5746697810243404e+002, 0., 3.1950000000000000e+002, 0.,
@@ -50,10 +72,22 @@ int unworked_triag(int argc, char** argv) {
 	cout << "Cam 1 pts: \n" << cam1pts << endl;
 	cout << "Cam 2 pts: \n" << cam2pts << endl;
 
+	cout << "\n hold your breath \n ---------- \n" << endl;
+
 	triangulatePoints(camera1, camera2, cam1pts, cam2pts, pts3d);
 
-	cout << "\n hold your breath \n ---------- \n" << endl;
 	cout << "Triangulated 3D pts: \n" << pts3d << endl;
+
+	cout << "\n hold your breath again \n ---------- \n" << endl;
+
+	Point3d one = Point3d(150, 100, 1);
+	Point3d two = Point3d(300, 200, 1);
+
+	Mat linear_sth = LinearLSTriangulation( one, camera1, two, camera2 );
+
+	cout << "LinearLSTriangulation 3D pt: \n" << linear_sth << endl;
 
 	return 0;
 }
+
+
